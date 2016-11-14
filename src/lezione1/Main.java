b30878e8a1a1d9d40c79a338efa00f607c525f5f
tss <tss@debian-tss24>
2016-11-14 09:19:19 +0100

    qualcosa

---
+++ src/lezione1/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package lezione1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class Main {
+    
+
+
+public static void main (String[] args){
+    System.out.println("primosw started...");
+    String frase =(JOptionPane.showInputDialog(" Inserisci una frase"+"\n"));
+    System.out.println("QUESTO È ."+ frase);
+
+}
+}
