84b4ed9586be1a81c15444c3432b7de4a906462b
tss <tss@debian-tss24>
2016-11-14 11:12:42 +0100

    tss lezione 2

---
+++ src/lezione2/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione2;
+
+/**
+ *
+ * @author tss
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // metodo alternativo per fare un commento
+        
+        
+        
+        
+    }
+    
+}
